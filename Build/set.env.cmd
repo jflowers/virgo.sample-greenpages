4d6f3b2000f4b9e4efafd94af9962e4d0c8376e9
Jay Flowers <jay.flowers@gmail.com>
2012-06-09 08:58:09 -0400

    add groovy and mvn env vars

--- Build/set.env.cmd
+++ Build/set.env.cmd
@@ -1,3 +1,5 @@
 SET JAVA_HOME=C:\Java\jdk1.7.0_4
 SET GRADLE_HOME=C:\Tools\gradle\gradle-1.0-rc-3
-SET PATH=%SystemRoot%\system32;%SystemRoot%;%SystemRoot%\System32\Wbem;%JAVA_HOME%\bin;%GRADLE_HOME%\bin;C:\Program Files\TortoiseGit\bin;C:\Program Files (x86)\Git\cmd;c:\opscode\chef\bin
+SET M2_HOME=C:\Tools\maven\apache-maven-2.2.1
+SET GROOVY_HOME=C:\Tools\groovy\groovy-1.8.6
+SET PATH=%SystemRoot%\system32;%SystemRoot%;%SystemRoot%\System32\Wbem;%JAVA_HOME%\bin;%M2_HOME%\bin;%GROOVY_HOME%\bin;%GRADLE_HOME%\bin;C:\Program Files\TortoiseGit\bin;C:\Program Files (x86)\Git\cmd;c:\opscode\chef\bin
