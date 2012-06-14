eb57dc34d6a4f2aca0442788d547a54bc2baf54a
Jay Flowers <jay.flowers@gmail.com>
2012-06-14 14:54:38 -0400

    correct paths

--- Build/chef/solo.rb
+++ Build/chef/solo.rb
@@ -1,10 +1,10 @@
 
 file_cache_path "c:/tools/chef/cache"
-cookbook_path [ File.dirname(__FILE__) + "../wokspace-management/Chef/cookbooks" ]
+cookbook_path [ File.dirname(__FILE__) + "/../workspace-management/Chef/cookbooks" ]
 file_backup_path "c:/tools/chef/backup"
 role_path "c:/tools/chef/roles"
 
-json_attribs File.dirname(__FILE__) + "../wokspace-management/Chef/node.json"
+json_attribs File.dirname(__FILE__) + "/../workspace-management/Chef/node.json"
 
 log_level :debug
 log_location STDOUT
