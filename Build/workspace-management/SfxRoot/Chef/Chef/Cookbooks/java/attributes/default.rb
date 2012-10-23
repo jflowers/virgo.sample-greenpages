31690533475a8ef2ac306a6376ffeeaff62be3d5
Jay Flowers <jay.flowers@gmail.com>
2012-10-23 09:55:23 -0400

    add all envs to bootstrap change java sdk download server

--- Build/workspace-management/SfxRoot/Chef/Chef/Cookbooks/java/attributes/default.rb
+++ Build/workspace-management/SfxRoot/Chef/Chef/Cookbooks/java/attributes/default.rb
@@ -11,7 +11,7 @@
 case platform
 when "windows"
   default['java']['java_home'] = 'C:\\Java\\jdk1.7.0_4'
-  default['java']['url'] = 'https://209.251.180.120/WebDav/Installs/jdk-7u4-windows-x64.exe'
+  default['java']['url'] = 'http://jayflowers.com/Misc%20Downloads/jdk-7u4-windows-x64.exe'
   default['java']['checksum'] = '203a0e484e25ac6b2950ec75de2c106c3aeaf115'
 else
 
--- Build/workspace-management/SfxRoot/run.bat
+++ Build/workspace-management/SfxRoot/run.bat
@@ -1,9 +1,10 @@
 
-SET PATH=%SystemRoot%\system32;%SystemRoot%;%SystemRoot%\System32\Wbem;C:\opscode\chef\bin;C:\Program Files (x86)\Git\cmd
+call set.env.cmd
 
 IF NOT EXIST C:\opscode\chef\bin\chef-solo.bat msiexec /qb /i chef-client-latest.msi
 
 call C:\opscode\chef\embedded\bin\gem.bat install minitest-chef-handler
+call C:\opscode\chef\embedded\bin\gem.bat update minitest
 
 mkdir C:\tools
 mkdir C:\tools\chef
